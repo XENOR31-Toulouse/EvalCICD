457f2a839bd36ce74985bb8a6d4afebeaa50a17d
XENOR31-Toulouse <axel.ibanez@my-digital-school.org>
2025-01-30 11:42:11 +0100

    test

--- cypress/e2e/test-to-do-list.cy.js
+++ cypress/e2e/test-to-do-list.cy.js
@@ -3,7 +3,8 @@
 describe('To Do List', () => {
   it('should add a new task to the list', () => {
     // j'ai creer un virtual host via wamp
-    cy.visit('http://to-do-list/');
+    
+    cy.visit('./index.html');
 
     cy.get('[data-cy=task-title-input]')
       .type('Task 1')
@@ -11,6 +12,8 @@ describe('To Do List', () => {
 
     cy.get('[data-cy=add-task-button]')
       .click();
+
+    cy.wait(500);
 
     cy.get('[data-cy=task-list]')
       .contains('Task 1');
